2e4db7e9e4a2069f48742db00ff349b11bf05e0b
seungji <kwag57@naver.com>
2021-04-14 21:32:08 +0900

    sqlite 적용

---
+++ Unknown Island Project(Unity Project)/Assets/DB/DBAccess.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using System.IO;
+using System.Data;
+using Mono.Data.Sqlite;
+
+
+
+public class DBAccess : MonoBehaviour
+{
+
+    private SqliteConnection m_DatabaseConnection;
+    private SqliteCommand m_DatabaseCommand;
+    private SqliteDataReader m_Reader;
+
+    public DBAccess(string connectionString)
+    {
+        OpenDatabase(connectionString);
+    }
+
+    public void OpenDatabase(string connectionString)
+    {
+        m_DatabaseConnection = new SqliteConnection(connectionString);
+        m_DatabaseConnection.Open();
+        Debug.Log("Connected to database");
+    }
+
+    public void CloseSqlConnection()
+    {
+        if (m_DatabaseCommand != null)
+        {
+            m_DatabaseCommand.Dispose();
+        }
+        m_DatabaseCommand = null;
+        if (m_Reader != null)
+        {
+            m_Reader.Dispose();
+        }
+        m_Reader = null;
+        if (m_DatabaseConnection != null)
+        {
+            m_DatabaseConnection.Close();
+        }
+        m_DatabaseConnection = null;
+        Debug.Log("Disconnected from database.");
+    }
+
+    public SqliteDataReader ExecuteQuery(string sqlQuery)
+    {
+        m_DatabaseCommand = m_DatabaseConnection.CreateCommand();
+        m_DatabaseCommand.CommandText = sqlQuery;
+
+        m_Reader = m_DatabaseCommand.ExecuteReader();
+
+        return m_Reader;
+    }
+
+    public SqliteDataReader ReadFullTable(string tableName)
+    {
+        string query = "SELECT * FROM " + tableName;
+        return ExecuteQuery(query);
+    }
+
+    public SqliteDataReader InsertInto(string tableName, string[] values)
+    {
+        string query = "INSERT INTO " + tableName + " VALUES (" + values[0];
+        for (int i = 1; i < values.Length; ++i)
+        {
+            query += ", " + values[i];
+        }
+        query += ")";
+        return ExecuteQuery(query);
+    }
+
+    public SqliteDataReader InsertIntoSpecific(string tableName, string[] cols, string[] values)
+    {
+        if (cols.Length != values.Length)
+        {
+            throw new SqliteException("columns.Length != values.Length");
+        }
+        string query = "INSERT INTO " + tableName + "(" + cols[0];
+        for (int i = 1; i < cols.Length; ++i)
+        {
+            query += ", " + cols[i];
+        }
+        query += ") VALUES (" + values[0];
+        for (int i = 1; i < values.Length; ++i)
+        {
+            query += ", " + values[i];
+        }
+        query += ")";
+        return ExecuteQuery(query);
+    }
+
+    public SqliteDataReader UpdateInto(string tableName, string[] cols, string[] colsvalues, string selectkey, string selectvalue)
+    {
+
+        string query = "UPDATE " + tableName + " SET " + cols[0] + " = " + colsvalues[0];
+
+        for (int i = 1; i < colsvalues.Length; ++i)
+        {
+
+            query += ", " + cols[i] + " =" + colsvalues[i];
+        }
+
+        query += " WHERE " + selectkey + " = " + selectvalue + " ";
+
+        return ExecuteQuery(query);
+    }
+
+    public SqliteDataReader DeleteContents(string tableName)
+    {
+        string query = "DELETE FROM " + tableName;
+        return ExecuteQuery(query);
+    }
+
+    public SqliteDataReader CreateTable(string name, string[] col, string[] colType)
+    {
+        if (col.Length != colType.Length)
+        {
+            throw new SqliteException("columns.Length != colType.Length");
+        }
+        string query = "CREATE TABLE " + name + " (" + col[0] + " " + colType[0];
+        for (int i = 1; i < col.Length; ++i)
+        {
+            query += ", " + col[i] + " " + colType[i];
+        }
+        query += ")";
+        return ExecuteQuery(query);
+    }
+
+    public SqliteDataReader SelectWhere(string tableName, string[] items, string[] col, string[] operation, string[] values)
+    {
+        if (col.Length != operation.Length || operation.Length != values.Length)
+        {
+            throw new SqliteException("col.Length != operation.Length != values.Length");
+        }
+        string query = "SELECT " + items[0];
+        for (int i = 1; i < items.Length; ++i)
+        {
+            query += ", " + items[i];
+        }
+        query += " FROM " + tableName + " WHERE " + col[0] + operation[0] + "'" + values[0] + "' ";
+        for (int i = 1; i < col.Length; ++i)
+        {
+            query += " AND " + col[i] + operation[i] + "'" + values[0] + "' ";
+        }
+
+        return ExecuteQuery(query);
+    }
+
+
+
+
+
+}
