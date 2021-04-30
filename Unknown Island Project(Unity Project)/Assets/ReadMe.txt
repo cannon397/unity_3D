517ea93d33b61b79bac1c53003c99b3bf302df66
Kimsudal <68053794+Kimsudal@users.noreply.github.com>
2021-05-01 00:48:59 +0900

    1

---
+++ Unknown Island Project(Unity Project)/Assets/ReadMe.txt
@@ -0,0 +1,19 @@
+
+/* Read Me */
+
+This package for setting up Animal Roar Sounds of NPC.
+
+So that, You can set up AudioSource Component.
+
+
+  - Setup Roar Sound for NPC -
+  
+  1) Select NPC in Hierarchy or Project Fold.
+  
+  2) Click "Add Component Button " of Inspector and Add "AudioSource"
+  
+  3) Drag and Drop to Audio Clip with Audio Clips ("BearAttacksnd, StagRoarSnd, TigerAttack Snd... etc") of "00.Sounds."
+  
+  
+  
+  
---
+++ Unknown Island Project(Unity Project)/Assets/zz.TESTRUN/testNPCRoarTEST.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class testNPCRoarTEST {
+
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator testNPCRoarTESTWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+
+        SetupScene();
+
+        yield return new WaitForSeconds(20);
+    }
+
+    void SetupScene()
+    {
+       MonoBehaviour.Instantiate(Resources.Load<GameObject>("Scenery"));
+    }
+
+}
